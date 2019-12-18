2b5003e40736df8ea511a08ecddb00084019a98d
Sarah B <smblair87@gmail.com>
2019-12-17 18:53:09 -0800

    Added search button

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
 
-function Navbar() {
+function Navbar({ children }) {
   return (
     <nav className="navbar">
       <img src="https://i.ibb.co/bsGcJRH/logo-symbol.png" alt="Helpful Human logo" border="0" ></img>
+      {children}
     </nav>
   );
 }
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';
-import Navbar from '../../components/Navbar';
-import { Container } from '../../components/Grid';
-import './style.css';
-import Footer from '../../components/Footer';
-
+import React, { Component } from "react";
+import Navbar from "../../components/Navbar";
+import { Container } from "../../components/Grid";
+import "./style.css";
+import Footer from "../../components/Footer";
 
 class Home extends Component {
-
-
   // Renders content onto main home page
   render() {
     return (
       <Container fluid>
-
-        <div className='row'>
-          <div className='col s12' id='paddingRemover'>
-
-          <Navbar></Navbar>
-    
+        <div className="row">
+          <div className="col s12" id="paddingRemover">
+            <Navbar>
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                ></input>
+                <button className="btn btn-outline-light" type="submit">
+                  Search
+                </button>
+              </form>
+            </Navbar>
           </div>
         </div>
 
-        <div className='row'>
-          <div className='col s12' id='paddingRemover'>
+        <div className="row">
+          <div className="col s12" id="paddingRemover">
             <Footer></Footer>
           </div>
         </div>
-
       </Container>
-    )
+    );
   }
 }
 
