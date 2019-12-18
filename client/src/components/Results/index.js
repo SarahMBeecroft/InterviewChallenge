a86deda8c66337218490a9ce3566fcf9a38da9c8
Sarah B <smblair87@gmail.com>
2019-12-17 19:53:17 -0800

    Created section for color results

---
+++ client/src/components/Results/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+
+function Results({ children }) {
+  return (
+    <div>
+      { children }
+    </div>
+  );
+}
+
+export default Results;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { Container } from "../../components/Grid";
 import Navbar from "../../components/Navbar";
 import ColorButtons from "../../components/ColorButtons";
+import Results from "../../components/Results";
 import "./style.css";
-import Footer from "../../components/Footer";
 
 class Home extends Component {
   // Renders content onto main home page
@@ -31,53 +31,55 @@ class Home extends Component {
         <div className="row appContent">
           <div className="col s4">
             <ColorButtons>
-              <button type="button" class="btn btn-outline-dark">
-                Random Color
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Red
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Orange
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Yellow
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Green
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Blue
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Purple
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Brown
-              </button>
-              <br></br>
-              <button type="button" class="btn btn-outline-dark">
-                Gray
-              </button>
-              <br></br>
+              <div className="colorButtons">
+                <button type="button" class="btn btn-dark">
+                  Random Color
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Red
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Orange
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Yellow
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Green
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Blue
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Purple
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Brown
+                </button>
+                <br></br>
+                <button type="button" class="btn btn-outline-dark">
+                  Gray
+                </button>
+                <br></br>
+              </div>
             </ColorButtons>
           </div>
 
           <div className="col s8">
-            <p>White space for now</p>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col s12" id="paddingRemover">
-            <Footer></Footer>
+            <Results>
+              <div className="colorResults"></div>
+              <br></br>
+              <button type="button" class="btn btn-dark">
+                Clear
+              </button>
+            </Results>
           </div>
         </div>
       </Container>
