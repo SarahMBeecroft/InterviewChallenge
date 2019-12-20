fe125345566286cacc347cbad75323d27042b07b
Sarah B <smblair87@gmail.com>
2019-12-20 06:47:21 -0800

    Troubleshooting deploy

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Ping route lets us test to make sure server is running
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
-
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
