266913fe9e870d048395f7d6a9fb461a7bc453cf
Sarah B <smblair87@gmail.com>
2019-12-19 16:51:33 -0800

    Added color card component for results

---
+++ client/src/components/ColorCard/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function ColorCard() {
+  return (
+    <div className="colorCard">
+      <div className="colorBox"></div>
+      <div className="colorText"></div>
+    </div>
+  );
+}
