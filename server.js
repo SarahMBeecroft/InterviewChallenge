f6e2b90dbfaef1a1bb95f3a50468a2397fe2f392
Sarah B <smblair87@gmail.com>
2019-12-19 20:07:06 -0800

    troubleshooting deploy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+
+
 // Defines middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
